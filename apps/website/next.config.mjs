ce61125e9c711fc628903fb402ac09e3341fb92e
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-09-07 07:12:21 +0800

    chore: add dockerfile for website

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -1,5 +1,8 @@
+const isDockerBuild = process.env.IS_DOCKER_BUILD === "true";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: isDockerBuild ? { standalone: true } : {},
     typescript: {
         ignoreBuildErrors: true,
     },

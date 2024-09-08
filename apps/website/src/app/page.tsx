6bbbf34ccda79b1b950658ce63bff91b1763f5ed
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-09-09 07:23:22 +0800

    feat: better page

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -1,13 +1,12 @@
-import { buttonVariants } from "@imperia/ui/components/ui/button";
-import { Card } from "@imperia/ui/components/ui/card";
+import { Badge } from "@imperia/ui/components/ui/badge";
+import { Button, buttonVariants } from "@imperia/ui/components/ui/button";
 import { Icons } from "@imperia/ui/components/ui/icons";
-import Image from "next/image";
-import Link from "next/link";
+import { cn } from "@imperia/ui/lib/utils";
 
 export default function Home() {
     return (
         <div className="min-h-screen flex flex-col md:flex-row gap-2 items-center justify-center">
-            <Card className="rounded-md invisible md:visible">
+            {/* <Card className="rounded-md invisible md:visible">
                 <Image src="/Imperia.png" width={120} height={120} alt="Picture of the author" />
             </Card>
             <div className="flex flex-col gap-2">
@@ -19,12 +18,27 @@ export default function Home() {
                         </p>
                     </div>
                 </Card>
-                <Card className="rounded-md border-none bg-none bg-transparent">
-                    <Link className={buttonVariants({ variant: "outline" })} href="">
-                        <Icons.github className="h-4 w-4 mr-2" />
+            </div> */}
+            <div className="flex flex-col items-center justify-center gap-2">
+                <Badge className="text-xs text-muted-foreground" variant="secondary">
+                    Coming Soon
+                </Badge>
+                <h1 className="text-4xl font-semibold leading-tight mt-2">Imperia</h1>
+                <p className="text-base font-light text-gray-300">All-in-one Discord bot solution for various tasks.</p>
+                <div className="mt-4 flex gap-2">
+                    <a
+                        href="https://github.com/yehezkieldio/imperia"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={cn(buttonVariants({ variant: "outline" }), "text-sm")}
+                    >
+                        <Icons.github className="w-4 h-4 mr-2" />
                         GitHub
-                    </Link>
-                </Card>
+                    </a>
+                    <Button variant="outline" className="px-8" disabled>
+                        Invite Me
+                    </Button>
+                </div>
             </div>
         </div>
     );

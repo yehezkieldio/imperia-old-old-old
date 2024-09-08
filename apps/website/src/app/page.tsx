b15279eea37a435965a959a1c4be81ab2a9b99ba
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-09-09 07:31:35 +0800

    feat: add privacy policy page

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -6,19 +6,6 @@ import { cn } from "@imperia/ui/lib/utils";
 export default function Home() {
     return (
         <div className="min-h-screen flex flex-col md:flex-row gap-2 items-center justify-center">
-            {/* <Card className="rounded-md invisible md:visible">
-                <Image src="/Imperia.png" width={120} height={120} alt="Picture of the author" />
-            </Card>
-            <div className="flex flex-col gap-2">
-                <Card className="rounded-md">
-                    <div className="text-left space-y-2 p-8 relative z-10">
-                        <h1 className="text-2xl leading-tight text-foreground font-bold max-w-4xl">Imperia</h1>
-                        <p className="text-sm font-light text-red-200">
-                            All-in-one Discord bot solution for various tasks.
-                        </p>
-                    </div>
-                </Card>
-            </div> */}
             <div className="flex flex-col items-center justify-center gap-2">
                 <Badge className="text-xs text-muted-foreground" variant="secondary">
                     Coming Soon
---
+++ apps/website/src/app/privacy-policy/page.tsx
@@ -0,0 +1,79 @@
+export default async function PrivacyPolicy() {
+    return (
+        <main className="flex-1 mb-24">
+            <section className="space-y-6 pt-6 md:pt-10 lg:pt-24">
+                <div className="container flex max-w-[64rem] flex-col gap-2 text-left">
+                    <div className="flex flex-col items-start gap-4">
+                        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Privacy</h1>
+                        <div className="mt-4">
+                            <p className="leading-7 [&:not(:first-child)]:mt-6 text-gray-300">
+                                Imperia collects and stores information about you when you use Imperia the bot or
+                                website. This information is used to provide you with the best experience and to improve
+                                the quality of our services.
+                            </p>
+                            <p className="leading-7 [&:not(:first-child)]:mt-8 text-gray-300">
+                                Please read the following to learn more about our Privacy Policy.
+                            </p>
+                            <h2 className="[&:not(:first-child)]:mt-8 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+                                1. Information We Collect
+                            </h2>
+                            <p className="leading-7 [&:not(:first-child)]:mt-6 text-gray-300">
+                                Imperia collects various Discord-related identifiers, including User IDs, Server IDs,
+                                Channel IDs, Role IDs, and Message IDs for the purpose of providing its functionalities.
+                                These identifiers are used exclusively within the Discord platform and are not shared or
+                                sold to third parties.
+                            </p>
+                            <p className="leading-7 [&:not(:first-child)]:mt-6 text-gray-300">
+                                Imperia may collect and store content provided by users for interaction or configuration
+                                purposes. This content includes but is not limited to command inputs, configurations,
+                                and customizations.
+                            </p>
+                            <p className="leading-7 [&:not(:first-child)]:mt-8 text-gray-300">
+                                We do not use this information for any purpose other than delivering the intended
+                                features of the bot.
+                            </p>
+                            <h2 className="[&:not(:first-child)]:mt-8 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+                                2. How We Use Your Information
+                            </h2>
+                            <p className="leading-7 [&:not(:first-child)]:mt-6 text-gray-300">
+                                We use the collected information to provide and enhance the features and functionalities
+                                of Imperia. This includes maintaining and improving bot performance, troubleshooting
+                                issues, and introducing new features based on user feedback.
+                            </p>
+                            <p className="leading-7 [&:not(:first-child)]:mt-6 text-gray-300">
+                                Imperia may use collected identifiers for communication purposes, such as responding to
+                                commands, providing updates on bot features, and addressing user inquiries. Users may
+                                choose to opt-out of non-essential communications.
+                            </p>
+                            <h2 className="[&:not(:first-child)]:mt-8 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+                                3. Data Security
+                            </h2>
+                            <p className="leading-7 [&:not(:first-child)]:mt-6 text-gray-300">
+                                We only retain collected information for as long as necessary to provide you with your
+                                requested service. What data we do collect, we’ll protect using commercially reasonable
+                                means to prevent loss and theft, as well as unauthorised access, disclosure, copying,
+                                use or modification.
+                            </p>
+                            <h2 className="[&:not(:first-child)]:mt-8 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+                                4. Information Sharing
+                            </h2>
+                            <p className="leading-7 [&:not(:first-child)]:mt-6 text-gray-300">
+                                Imperia does not share user data with third-party services, except for Discord, which is
+                                integral to the bot&lsquo;s operation. Imperia may link to external sites that are not
+                                operated by us. Please be aware that we have no control over the content and practices
+                                of these sites, and cannot accept responsibility or liability for their respective
+                                privacy policies.
+                            </p>
+                            <p className="leading-7 [&:not(:first-child)]:mt-10 text-gray-300">
+                                By using Imperia, you agree to the terms outlined in this Privacy Policy. If you do not
+                                agree with these terms, please refrain from using the bot. If you have any questions
+                                about this Privacy Policy, please contact the lead developer of Imperia via the official
+                                support server.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
+}
